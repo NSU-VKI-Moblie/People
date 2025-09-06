3b352ebf23b0785e761191ea16c7df3d0e21cd75
gusevma <gusevma@mer.ci.nsu.ru>
2025-09-06 11:11:53 +0700

    123123

--- app/src/main/java/ci/nsu/moble/people/MainActivity.kt
+++ app/src/main/java/ci/nsu/moble/people/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PeopleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "An1212droid",
+                        name = "An12121112droid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
