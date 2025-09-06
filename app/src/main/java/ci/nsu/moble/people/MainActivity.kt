f09b3b8669e8f9042199e1f2dd4b275949d3192f
ManyahinFE <fedormanahin7142@gmail.com>
2025-09-06 10:34:12 +0700

    123

--- app/src/main/java/ci/nsu/moble/people/MainActivity.kt
+++ app/src/main/java/ci/nsu/moble/people/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PeopleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "BavarskieKolbaski",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
